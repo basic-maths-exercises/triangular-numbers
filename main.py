c198b07c60ed5778de131faec40b6ac51d7930bc
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-06 09:46:29 +0100

    Added answers

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import numpy as np
 xvals = np.zeros(100)
 triangularNumbers = np.zeros(100)
 # Insert your code to compute the triangular numbers here
-
+for i in range(1,100) : 
+    xvals[i]=i
+    triangularNumbers[i] = triangularNumbers[i-1] + i
 
 
 plt.plot( xvals, triangularNumbers, 'ko' )
